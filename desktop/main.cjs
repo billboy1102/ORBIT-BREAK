b7167a87f176dc67f7c0ce3398779f4a02e06fd1
billboy1102 <mabasuke23@gmail.com>
2026-08-13 12:52:10 +0700

    Add Electron Windows wrapper

---
+++ desktop/main.cjs
@@ -0,0 +1,38 @@
+const { app, BrowserWindow, shell } = require('electron');
+const path = require('path');
+
+app.setAppUserModelId('com.orbitbreak.game');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 600,
+    height: 980,
+    minWidth: 420,
+    minHeight: 700,
+    backgroundColor: '#050711',
+    autoHideMenuBar: true,
+    title: 'ORBIT BREAK',
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: true
+    }
+  });
+
+  win.loadFile(path.join(__dirname, '..', 'index.html'));
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    if (/^https?:\/\//i.test(url)) shell.openExternal(url);
+    return { action: 'deny' };
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
